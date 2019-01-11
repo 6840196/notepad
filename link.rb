5b94b957e9aab1f48217f3d2621dbd8208d95ba7
linoge <6840196@gmail.com>
2019-01-11 13:49:24 +0300

    Notepad v.1 Working

--- link.rb
+++ link.rb
@@ -7,10 +7,17 @@ class Link < Post
   end
 
   def read_from_console
+    puts "Введите адрес ссылки"
+    @url = STDIN.gets.chomp
+
+    puts "Введите описание ссылки"
+    @text = STDIN.gets.chomp
 
   end
 
   def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y-%m-%d, %H:%M:%S")}"
 
+    return [@url, @text, time_string]
   end
 end
--- task.rb
+++ task.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Task < Post
 
   def initialize
@@ -7,10 +9,20 @@ class Task < Post
   end
 
   def read_from_console
+    puts "Введите имя задачи"
+    @text = STDIN.gets.chomp
 
+    puts "Введите дату дедлайна"
+    input = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
   end
 
   def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y-%m-%d, %H:%M:%S")}"
 
+    deadline = "Крайний срок: #{@due_date}"
+
+    return [deadline, @text, time_string]
   end
 end
